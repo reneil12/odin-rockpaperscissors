8964323a8ddbc21418016144d0205adf9b3987c0
reneil12 <reneilrepollo@gmail.com>
2023-10-31 23:49:25 +0800

    Finished the js code, style in progress

--- script.js
+++ script.js
@@ -12,83 +12,113 @@ function getComputerChoice(){
           return computerChoice;
     }
 }
-function getPlayerChoice(){
-      let playerChoice = prompt("Rock, Paper, or Scissors?")
-      return playerChoice.toLowerCase();
-}
+
+const playerChoice = document.querySelectorAll(".btn");
+    playerChoice.forEach((pChoice) =>  {
+        function getPlayerChoice() {
+            console.log(pChoice.value);
+            player = pChoice.value;
+            game();
+        }
+    pChoice.addEventListener('click', getPlayerChoice);
+});
+
+const resetButton = document.querySelector('.reset-btn')
+resetButton.disabled = true;
+resetButton.addEventListener('click', () => {
+    playerScore = 0;
+    computerScore = 0;
+    resultText.textContent = originalResultText;
+    scoreText.textContent = originalScoreText;
+    winnerText.textContent = originalWinnerText;
+    resetButton.disabled = true;
+        playerChoice.forEach((button) => {
+            button.disabled = false;
+        })
+
+})
+
 let player;
 let computer;
 let playerScore = 0;
 let computerScore = 0;
+const resultText = document.querySelector('.result');
+const scoreText = document.querySelector('.score');
+const winnerText = document.querySelector('.winner');
+const originalResultText = resultText.textContent;
+const originalScoreText = scoreText.textContent;
+const originalWinnerText = winnerText.textContent;
+
+
 function playRound(){
     switch(player){
         case 'rock':
             if (computer === 'scissors'){
-                alert("You win! Rock beats Scissors!")
+                resultText.textContent = "You win! Rock beats Scissors!";
                 playerScore += 1;
                 break;
             }
             else if (computer === 'paper'){
-                alert("You lose! Paper beats Rock!")
+                resultText.textContent = "You lose! Paper beats Rock!";
                 computerScore += 1;
                 break;
             }
             else {
-                  alert("Draw!");
+                resultText.textContent = "Draw!";
                   break;
             }
           case 'paper':
             if (computer === 'scissors') {
-                alert("You lose! Scissors beat Paper!")
+                resultText.textContent = "You lose! Scissors beat Paper!"
                 computerScore += 1;
                 break;
             }
             else if (computer === 'rock') {
-                alert("You win! Paper beats Rock!")
+                resultText.textContent = "You win! Paper beats Rock!"
                 playerScore += 1;
                 break;
             }
             else {
-                alert("Draw!");
+                resultText.textContent = "Draw!";
                 break;
             }
           case 'scissors':
             if (computer === 'paper') {
-                alert("You win! Scissors beat Paper!")
+                resultText.textContent = "You win! Scissors beat Paper!"
                 playerScore += 1;
                 break
             }
             else if (computer === 'rock') {
-                alert("You lose! Rock beats Scissors")
+                resultText.textContent = "You lose! Rock beats Scissors"
                 computerScore += 1;
                 break;
             }
             else {
-                alert("Draw!");
+                resultText.textContent = "Draw!";
                 break;
             }
     }
-    console.log(`Player: ${playerScore} Computer: ${computerScore}`)
+    scoreText.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
 }
 function determineWinner(){
-    if (playerScore > computerScore){
-        console.log("You Win!");
-    }
-    else if (playerScore < computerScore) {
-        console.log("You Lose!");
-    }
-    else {
-          console.log("Draw!");
-    }
+    if (playerScore === 5 || computerScore === 5){
+        resetButton.disabled = false;
+        playerChoice.forEach((button) => {
+            button.disabled = true;
+        })
+
+        if (playerScore > computerScore){
+            winnerText.textContent = "Player Wins!";
+        }
+        else {
+            winnerText.textContent = "Computer Wins!";
+        }
+    }   
 }
 function game(){
-    for (let i = 1; i <= 5; i++){
-        console.log(`Round ${i}: `);
-        player = getPlayerChoice();
-        computer = getComputerChoice();
-        playRound();
-    }
+    computer = getComputerChoice();
+    playRound();
     determineWinner();
 }
 
-game();
+
